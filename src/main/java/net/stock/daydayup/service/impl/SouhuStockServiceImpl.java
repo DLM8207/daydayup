a35e02cdf9682ae500145f4bf62679755c981940
dailiming <dailm@chinatelecom.cn>
2022-09-06 09:04:11 +0800

    upate patch implements

--- src/main/java/net/stock/daydayup/service/impl/SouhuStockServiceImpl.java
+++ src/main/java/net/stock/daydayup/service/impl/SouhuStockServiceImpl.java
@@ -6,18 +6,14 @@ package net.stock.daydayup.service.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.stock.daydayup.bean.ObjectEntity;
 import net.stock.daydayup.bean.StockValueEntity;
 import net.stock.daydayup.bean.SynObjectListEntity;
 import net.stock.daydayup.constant.CommonData;
-import net.stock.daydayup.repository.ObjectEntityRepository;
 import net.stock.daydayup.repository.StockValueRepository;
 import net.stock.daydayup.repository.SynObjectListRepository;
-import net.stock.daydayup.service.ObjectService;
 import net.stock.daydayup.service.SouhuStockService;
 import net.stock.daydayup.util.HttpUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -27,7 +23,9 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author:dailm
@@ -42,70 +40,77 @@ public class SouhuStockServiceImpl implements SouhuStockService {
 
     @Override
     public void downloadHistoryData(int year) {
+        ThreadPoolExecutor tpe = new ThreadPoolExecutor(20,200,30, TimeUnit.HOURS, new ArrayBlockingQueue<>(50));
         long count = synObjectListRepository.count();
-        int pageSize = 200;
+        int pageSize = 100;
         long pageNum = count / pageSize;
         if(count%pageSize!=0){
             pageNum++;
         }
+        List<Integer> finsishIndex = new ArrayList<>();
         for(int i=0;i<pageNum;i++) {
-            Pageable pageable = PageRequest.of((i + 1), pageSize);
-            Page<SynObjectListEntity> pages = synObjectListRepository.findAll(pageable);
-            if (!pages.isEmpty()) {
-                Iterator<SynObjectListEntity> iterator = pages.iterator();
-                while (iterator.hasNext()) {
-                    SynObjectListEntity objectEntity = iterator.next();
-                    String code = objectEntity.getCode();
-                    String respData = getSohuData(code,year);
-                    List<String> finishDays = stockValueRepository.findDayByCodeAndYear(code,year+"-01-01");
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    try {
-                        if(respData==null||"".equals(respData))
-                            continue;
-                        JsonNode jsonNode = objectMapper.readValue(respData, JsonNode.class);
-                        JsonNode dateValues = jsonNode.get("hq");
-                        if(dateValues==null)
-                            continue;
-                        Iterator<JsonNode> iterator2 = dateValues.iterator();
-                        List<StockValueEntity> savingList = new ArrayList<>();
-                        while (iterator2.hasNext()){
-                            //日期、开盘价、收盘价、涨跌额、涨跌幅、最低价、最高价、总手、总成交额、换手率
-                            JsonNode node = iterator2.next();
-                            String strDate = node.get(0).asText();
-                            //当前股票，该日期值已经存在则跳过
-                            if(finishDays!=null&&finishDays.indexOf(strDate)!=-1)
+            final int index = i;
+            tpe.execute(()->{
+                Pageable pageable = PageRequest.of((index + 1), pageSize);
+                Page<SynObjectListEntity> pages = synObjectListRepository.findAll(pageable);
+                if (!pages.isEmpty()) {
+                    Iterator<SynObjectListEntity> iterator = pages.iterator();
+                    while (iterator.hasNext()) {
+                        SynObjectListEntity objectEntity = iterator.next();
+                        String code = objectEntity.getCode();
+                        String respData = getSohuData(code,year);
+                        List<String> finishDays = stockValueRepository.findDayByCodeAndYear(code,year+"-01-01");
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        try {
+                            if(respData==null||"".equals(respData))
                                 continue;
-                            String strOpen = node.get(1).asText();
-                            String strClose = node.get(2).asText();
-                            String strChangeAmt = node.get(3).asText();
-                            String strChangeRate = node.get(4).asText();
-                            String strLow = node.get(5).asText();
-                            String strHeight = node.get(6).asText();
-                            String strCount = node.get(7).asText();
-                            String strAmt = node.get(8).asText();
-                            String strTNU = node.get(9).asText();
-                            StockValueEntity stockValueEntity = new StockValueEntity();
-                            stockValueEntity.setDay(Date.valueOf(strDate));
-                            stockValueEntity.setClose(Double.valueOf(strClose));
-                            stockValueEntity.setOpen(Double.valueOf(strOpen));
-                            stockValueEntity.setHeight(Double.valueOf(strHeight));
-                            stockValueEntity.setLower(Double.valueOf(strLow));
-                            stockValueEntity.setVolume(Long.valueOf(strCount));
-                            stockValueEntity.setTurnover(Double.valueOf(strAmt));
-                            stockValueEntity.setAmtIncDec(Double.valueOf(strChangeAmt));
-                            stockValueEntity.setIncDecRate(strChangeRate);
-                            stockValueEntity.setTurnoverRate(strTNU);
-                            stockValueEntity.setStockcode(code);
-                            savingList.add(stockValueEntity);
+                            JsonNode jsonNode = objectMapper.readValue(respData, JsonNode.class);
+                            JsonNode dateValues = jsonNode.get("hq");
+                            if(dateValues==null)
+                                continue;
+                            Iterator<JsonNode> iterator2 = dateValues.iterator();
+                            List<StockValueEntity> savingList = new ArrayList<>();
+                            while (iterator2.hasNext()){
+                                //日期、开盘价、收盘价、涨跌额、涨跌幅、最低价、最高价、总手、总成交额、换手率
+                                JsonNode node = iterator2.next();
+                                String strDate = node.get(0).asText();
+                                //当前股票，该日期值已经存在则跳过
+                                if(finishDays!=null&&finishDays.indexOf(strDate)!=-1)
+                                    continue;
+                                String strOpen = node.get(1).asText();
+                                String strClose = node.get(2).asText();
+                                String strChangeAmt = node.get(3).asText();
+                                String strChangeRate = node.get(4).asText();
+                                String strLow = node.get(5).asText();
+                                String strHeight = node.get(6).asText();
+                                String strCount = node.get(7).asText();
+                                String strAmt = node.get(8).asText();
+                                String strTNU = node.get(9).asText();
+                                StockValueEntity stockValueEntity = new StockValueEntity();
+                                stockValueEntity.setDay(Date.valueOf(strDate));
+                                stockValueEntity.setClose(Double.valueOf(strClose));
+                                stockValueEntity.setOpen(Double.valueOf(strOpen));
+                                stockValueEntity.setHeight(Double.valueOf(strHeight));
+                                stockValueEntity.setLower(Double.valueOf(strLow));
+                                stockValueEntity.setVolume(Long.valueOf(strCount));
+                                stockValueEntity.setTurnover(Double.valueOf(strAmt));
+                                stockValueEntity.setAmtIncDec(Double.valueOf(strChangeAmt));
+                                stockValueEntity.setIncDecRate(strChangeRate);
+                                stockValueEntity.setTurnoverRate(strTNU);
+                                stockValueEntity.setStockcode(code);
+                                savingList.add(stockValueEntity);
+                            }
+                            if(savingList.size()>0){
+                                stockValueRepository.saveAll(savingList);
+                            }
+                        } catch (JsonProcessingException e) {
+                            e.printStackTrace();
                         }
-                        if(savingList.size()>0){
-                            stockValueRepository.saveAll(savingList);
-                        }
-                    } catch (JsonProcessingException e) {
-                        e.printStackTrace();
                     }
                 }
-            }
+                finsishIndex.add(index);
+                finsishIndex.forEach((item)->{System.out.print(item+" ");});
+            });
         }
     }
 
